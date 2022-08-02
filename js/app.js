8aab481f1d90fa8655db327b0b97e4081f6745a5
FabioLongoni <longoni.fabio90@gmail.com>
2022-08-02 09:28:24 +0200

    add-bomb-generator-bomb-position

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 //richiamiamo il container
 const gridContainer = document.querySelector('.grid');
 //richiamiamo il button
@@ -7,27 +5,52 @@ const playButton = document.getElementById('play_button');
 //richiamare select
 const levelSelectElement = document.querySelector('select[name="difficult"]');
 
-playButton.addEventListener('click', makeGrid) 
-  
+playButton.addEventListener('click', startGame) ;
+
+let bombPosition = [];
     
-function makeGrid() {
+function startGame() {
 
     resetGame();
     const levelDifficult = levelSelectElement.value
     console.log(levelDifficult);
 
-    let dimension = levelGridCalc(levelDifficult);
-    let gridSize = dimension **2 ;
+    let dimension = gridCalc(levelDifficult);
+    
+    bombPosition = bombGenerator(dimension ** 2);
+    console.log(bombPosition);
+    
+    makeGrid(dimension);
+    
+}
+
+function makeGrid(gridDimension) {
+
+    const gridSize = gridDimension ** 2;
 
     for (let i = 0; i < gridSize; i++) {
         const cell = getSquareElement();
         
-        cell.style.flexBasis = `${100 / dimension}%`
+        cell.style.flexBasis = `${100 / gridDimension}%`
         cell.innerHTML = i + 1;
         gridContainer.append(cell);
     }
-    
+
 }
+
+function bombGenerator(max) {
+    const bomb = [];
+
+    while (bomb.length < 16 ) {
+        const n = getRandomIntInclusive(1, max);
+        if(!bomb.includes(n)) {
+            bomb.push(n);
+        }
+    }
+
+    return bomb
+}
+
 
 function getSquareElement() {
     const square = document.createElement('div');
@@ -38,16 +61,23 @@ function getSquareElement() {
 }
 
 function clickHover() {
-    const square = this;
-    square.classList.toggle('click_hover');
-    console.log(square.innerHTML);
+    // this.classList.toggle('click_hover');
+    const cellNumber = parseInt(this.innerHTML);
+    // console.log(cellNumber);
+    // console.log(bombPosition.includes(cellNumber));
+    let className = 'success';
+    if (bombPosition.includes(cellNumber)) {
+        className = 'bomb';
+    }
+
+    this.classList.add(className);
 }
 
 function resetGame() {
     gridContainer.innerHTML = '';
 }
 
-function levelGridCalc (levelDifficult) { 
+function gridCalc (levelDifficult) { 
 
     let dimension = 10;
 
@@ -58,4 +88,10 @@ function levelGridCalc (levelDifficult) {
     }
 
     return dimension;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
